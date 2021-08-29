afbf6784aa523bee9686f43cdd82452d7aa56c45
Mia Moretti <mimickal.dev@gmail.com>
2021-08-29 11:14:52 -0400

    Rename project to Zerda, because that name is cuter

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /*******************************************************************************
- * This file is part of PlayingBot, a role-assigning Discord bot.
+ * This file is part of Zerda, a role-assigning Discord bot.
  * Copyright (C) 2020 Mimickal (Mia Moretti).
  *
- * PlayingBot is free software under the GNU Affero General Public License v3.0.
+ * Zerda is free software under the GNU Affero General Public License v3.0.
  * See LICENSE or <https://www.gnu.org/licenses/agpl-3.0.en.html> for more
  * information.
  ******************************************************************************/

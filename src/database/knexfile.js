0457aae9c69f656e5206ad126a6d5535260c19f8
Mia Moretti <mimickal.dev@gmail.com>
2021-10-05 01:02:34 -0400

    Initial knex and sqlite setup

---
+++ src/database/knexfile.js
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * This file is part of Zerda, a role-assigning Discord bot.
+ * Copyright (C) 2020 Mimickal (Mia Moretti).
+ *
+ * Zerda is free software under the GNU Affero General Public License v3.0.
+ * See LICENSE or <https://www.gnu.org/licenses/agpl-3.0.en.html> for more
+ * information.
+ ******************************************************************************/
+const config = require('../config');
+
+// Paths are all relative to this JS file.
+const DEFAULT_DB_FILE = '../../dev.sqlite3';
+
+module.exports = {
+	client: 'sqlite3',
+	connection: {
+		filename: config.filename ?? DEFAULT_DB_FILE,
+	},
+	migrations: {
+		directory: './migrations',
+	},
+};
